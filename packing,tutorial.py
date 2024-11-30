c4bc167830b74c6a0286a72eb80161e26ec8b5b3
Lightromax <calendara132@gmail.com>
2024-11-30 16:47:56 +0000

    Add files via upload

---
+++ packing,tutorial.py
@@ -0,0 +1,8 @@
+my_Tupil=("what would,you like","cherries",45,"iron_maniac")
+print(my_Tupil[0])
+print(my_Tupil[1:3])
+my_tupil=("cherry","cucmber","tomato","apple")
+jetlearn=my_Tupil + my_tupil
+print(jetlearn)
+print(my_Tupil*3)
+print(len(jetlearn))
